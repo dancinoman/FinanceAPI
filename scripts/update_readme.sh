55be0a82ea896e875a4f3e79c373a7e9a38b94e1
Hugo Guay <dancinoman@gmail.com>
2025-05-10 18:40:52 -0400

    fix intial version tracking

--- scripts/update_readme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VERSION=$(cat VERSION)
-
-# Restore original placeholder first (optional, only needed if not using template)
-sed -i 's/Current Version: `.*`/Current Version: `{{VERSION}}`/' README.md
-
-# Replace placeholder with actual version
-sed -i "s/{{VERSION}}/$VERSION/" README.md
